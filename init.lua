441894611cdc8ad315d8dc3a520aec13473bde06
David Rambo <davidrambo@mailfence.com>
2023-04-03 13:01:24 -0700

    Add mini.indentscope

--- init.lua
+++ init.lua
@@ -72,10 +72,31 @@ require("lazy").setup({
     opts = {
       char = "┊",
       space_char_blankline = " ",
-      show_current_context = true,
+      show_current_context = false,
       show_trailing_blankline_indent = false,
       filetype_exclude = { "dashboard", "help", "NvimTree" },
     },
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    version = false,
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "help", "dashboard", "NvimTree", "lazy", "mason" },
+        callback = function()
+          vim.b.miniidentscope_disable = true
+        end,
+      })
+    end,
+    config = function(_, opts)
+      require("mini.indentscope").setup(opts)
+    end,
   },
 
   -- "gc" to comment visual regions/lines
