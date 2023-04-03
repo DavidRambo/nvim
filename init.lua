0ffe3db8ebe4ec41c3d9793b2ea1cb7f6404199f
David Rambo <davidrambo@mailfence.com>
2023-04-03 08:29:10 -0700

    select = false for <CR> in nvim-cmp

--- init.lua
+++ init.lua
@@ -407,7 +407,7 @@ cmp.setup({
     ["<C-Space>"] = cmp.mapping.complete({}),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      select = false,
     }),
     -- ["<Tab>"] = cmp.mapping(function(fallback)
     --   if cmp.visible() then
