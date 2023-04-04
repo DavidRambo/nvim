2684d6d0e605c08a884d106ef05313da23fdc3b8
David Rambo <davidrambo@mailfence.com>
2023-04-04 09:02:04 -0700

    Add event handler to indent-blankline

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,9 +1,8 @@
 return {
 	{
-		-- Add indentation guides even on blank lines
 		"lukas-reineke/indent-blankline.nvim",
-		-- Enable `lukas-reineke/indent-blankline.nvim`
 		-- See `:help indent_blankline.txt`
+		event = { "BufReadPost", "BufNewFile" },
 		opts = {
 			char = "┊",
 			space_char_blankline = " ",
