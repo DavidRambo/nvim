0d8be0d202c5febbfde762b3add0eaddda360ee9
David Rambo <davidrambo@mailfence.com>
2023-04-04 13:52:44 -0700

    Add additional filetypes for indent-blankline to exclude

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -8,7 +8,7 @@ return {
 			space_char_blankline = " ",
 			show_current_context = false,
 			show_trailing_blankline_indent = false,
-			filetype_exclude = { "dashboard", "help", "NvimTree" },
+			filetype_exclude = { "dashboard", "help", "NvimTree", "lazy", "mason" },
 		},
 	},
 
