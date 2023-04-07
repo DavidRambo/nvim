5645f9c0f795b54e76ff024db925127b71c28584
David Rambo <davidrambo@mailfence.com>
2023-04-07 07:53:13 -0700

    Add InsertEnter event to tabout.

--- lua/plugins/tabout.lua
+++ lua/plugins/tabout.lua
@@ -1,15 +1,16 @@
 return {
 	"abecodes/tabout.nvim",
+	event = "InsertEnter",
 	config = function()
 		require("tabout").setup({
-			tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+			tabkey = "<Tab>",       -- key to trigger tabout, set to an empty string to disable
 			backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
-			act_as_tab = true, -- shift content if tab out is not possible
+			act_as_tab = true,      -- shift content if tab out is not possible
 			act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-			default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+			default_tab = "<C-t>",  -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
 			default_shift_tab = "<C-d>", -- reverse shift default action,
 			enable_backwards = true, -- well ...
-			completion = true, -- if the tabkey is used in a completion pum
+			completion = true,      -- if the tabkey is used in a completion pum
 			tabouts = {
 				{ open = "'", close = "'" },
 				{ open = '"', close = '"' },
