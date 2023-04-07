23aee1facd6d08ee512009f0973ef585dbd70acd
David Rambo <davidrambo@mailfence.com>
2023-04-07 09:24:11 -0700

    autoformat from lspconfig to null-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,234 +1,170 @@
 return {
 
-	-- NOTE: This is where your plugins related to LSP can be installed.
-	--  The configuration is done below. Search for lspconfig to find it below.
-	{
-		-- LSP Configuration & Plugins
-		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			-- Automatically install LSPs to stdpath for neovim
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  {
+    -- LSP Configuration & Plugins
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
 
-			-- Useful status updates for LSP
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-			{ "j-hui/fidget.nvim", opts = {} },
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      { "j-hui/fidget.nvim", opts = {} },
 
-			-- Additional lua configuration, makes nvim stuff amazing!
-			"folke/neodev.nvim",
-		},
-		opts = {
-			diagnostics = {
-				float = {
-					source = "always",
-				},
-				virtual_text = false,
-			},
-			autoformat = true,
-			servers = {
-				clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- tsserver = {},
-				-- pylsp = {
-				-- 	pylsp = {
-				-- 		plugins = {
-				-- 			black = { enabled = true },
-				-- 			-- disable linting in favor of ruff_lsp
-				-- 			mccabe = { enabled = false },
-				-- 			pycodestyle = {
-				-- 				enabled = false,
-				-- 				-- ignore = { "W391", "E226", "E501" },
-				-- 				-- maxLineLength = 88,
-				-- 			},
-				-- 			pyflakes = { enabled = false },
-				-- 			pylint = { enabled = false },
-				-- 		},
-				-- 	},
-				-- },
-				jedi_language_server = {
-					init_options = {
-						diagnostics = { enable = false },
-					},
-				},
-				ruff_lsp = {},
-				lua_ls = {
-					Lua = {
-						workspace = { checkThirdParty = false },
-						telemetry = { enable = false },
-					},
-				},
-			},
-		},
-		config = function(_, opts)
-			-- Switch for controlling whether you want autoformatting.
-			--  Use :KickstartFormatToggle to toggle autoformatting on or off
-			local format_is_enabled = true
-			vim.api.nvim_create_user_command("KickstartFormatToggle", function()
-				format_is_enabled = not format_is_enabled
-				print("Setting autoformatting to: " .. tostring(format_is_enabled))
-			end, {})
+      -- Additional lua configuration, makes nvim stuff amazing!
+      "folke/neodev.nvim",
+    },
+    opts = {
+      diagnostics = {
+        float = {
+          source = "always",
+        },
+        virtual_text = false,
+      },
+      -- autoformat = true,
+      servers = {
+        clangd = {},
+        -- gopls = {},
+        -- pyright = {},
+        -- rust_analyzer = {},
+        -- tsserver = {},
+        -- pylsp = {
+        -- 	pylsp = {
+        -- 		plugins = {
+        -- 			black = { enabled = true },
+        -- 			-- disable linting in favor of ruff_lsp
+        -- 			mccabe = { enabled = false },
+        -- 			pycodestyle = {
+        -- 				enabled = false,
+        -- 				-- ignore = { "W391", "E226", "E501" },
+        -- 				-- maxLineLength = 88,
+        -- 			},
+        -- 			pyflakes = { enabled = false },
+        -- 			pylint = { enabled = false },
+        -- 		},
+        -- 	},
+        -- },
+        jedi_language_server = {
+          init_options = {
+            diagnostics = { enable = false },
+          },
+        },
+        ruff_lsp = {},
+        lua_ls = {
+          Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      local on_attach = function(client, bufnr)
+        -- Attach the server to Navbuddy only if server is not ruff_lsp.
+        -- ruff_lsp does not support documentSymbols.
+        if client.name ~= "ruff_lsp" then
+          require("nvim-navbuddy").attach(client, bufnr)
+        end
 
-			-- Create an augroup that is used for managing our formatting autocmds.
-			--      We need one augroup per client to make sure that multiple clients
-			--      can attach to the same buffer without interfering with each other.
-			local _augroups = {}
-			local get_augroup = function(client)
-				if not _augroups[client.id] then
-					local group_name = "kickstart-lsp-format-" .. client.name
-					local id = vim.api.nvim_create_augroup(group_name, { clear = true })
-					_augroups[client.id] = id
-				end
+        -- In this case, we create a function that lets us more easily define mappings specific
+        -- for LSP related items. It sets the mode, buffer and description for us each time.
+        local nmap = function(keys, func, desc)
+          if desc then
+            desc = "LSP: " .. desc
+          end
 
-				return _augroups[client.id]
-			end
+          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+        end
 
-			-- Whenever an LSP attaches to a buffer, we will run this function.
-			--
-			-- See `:help LspAttach` for more information about this autocmd event.
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("kickstart-lsp-attach-format", { clear = true }),
-				-- This is where we attach the autoformatting for reasonable clients
-				callback = function(args)
-					local client_id = args.data.client_id
-					local client = vim.lsp.get_client_by_id(client_id)
-					local bufnr = args.buf
+        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+        nmap("<C-c>", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-					-- Only attach to clients that support document formatting
-					if not client.server_capabilities.documentFormattingProvider then
-						return
-					end
+        nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+        nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+        nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+        -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-					-- Tsserver usually works poorly. Sorry you work with bad languages
-					-- You can remove this line if you know what you're doing :)
-					-- if client.name == "tsserver" then
-					-- 	return
-					-- end
+        -- See `:help K` for why this keymap
+        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+        nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-					-- Create an autocmd that will run *before* we save the buffer.
-					--  Run the formatting command for the LSP that has just attached.
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = get_augroup(client),
-						buffer = bufnr,
-						callback = function()
-							if not format_is_enabled then
-								return
-							end
+        -- Lesser used LSP functionality
+        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+        -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+        -- nmap('<leader>wl', function()
+        --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        -- end, '[W]orkspace [L]ist Folders')
 
-							vim.lsp.buf.format({
-								async = false,
-								filter = function(c)
-									return c.id == client.id
-								end,
-							})
-						end,
-					})
-				end,
-			})
-			local on_attach = function(client, bufnr)
-				-- Attach the server to Navbuddy only if server is not ruff_lsp.
-				-- ruff_lsp does not support documentSymbols.
-				if client.name ~= "ruff_lsp" then
-					require("nvim-navbuddy").attach(client, bufnr)
-				end
+        -- Create a command `:Format` local to the LSP buffer
+        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+          vim.lsp.buf.format()
+        end, { desc = "Format current buffer with LSP" })
+        nmap("<leader>bf", "<cmd>Format<CR>", "[B]uffer [F]ormat")
+      end
 
-				-- In this case, we create a function that lets us more easily define mappings specific
-				-- for LSP related items. It sets the mode, buffer and description for us each time.
-				local nmap = function(keys, func, desc)
-					if desc then
-						desc = "LSP: " .. desc
-					end
+      require("neodev").setup()
 
-					vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-				end
+      vim.diagnostic.config(opts.diagnostics)
 
-				nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-				nmap("<C-c>", vim.lsp.buf.code_action, "[C]ode [A]ction")
+      -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-				nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-				nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-				nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-				nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-				nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-				-- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+      -- Ensure the servers above are installed
+      local mason_lspconfig = require("mason-lspconfig")
 
-				-- See `:help K` for why this keymap
-				nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-				nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+      mason_lspconfig.setup({
+        ensure_installed = vim.tbl_keys(opts.servers),
+      })
 
-				-- Lesser used LSP functionality
-				nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-				-- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-				-- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-				-- nmap('<leader>wl', function()
-				--   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-				-- end, '[W]orkspace [L]ist Folders')
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          require("lspconfig")[server_name].setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = opts.servers[server_name],
+          })
+        end,
+      })
+    end,
+  },
 
-				-- Create a command `:Format` local to the LSP buffer
-				vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-					vim.lsp.buf.format()
-				end, { desc = "Format current buffer with LSP" })
-				nmap("<leader>bf", "<cmd>Format<CR>", "[B]uffer [F]ormat")
-			end
-
-			require("neodev").setup()
-
-			vim.diagnostic.config(opts.diagnostics)
-
-			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-			-- Ensure the servers above are installed
-			local mason_lspconfig = require("mason-lspconfig")
-
-			mason_lspconfig.setup({
-				ensure_installed = vim.tbl_keys(opts.servers),
-			})
-
-			mason_lspconfig.setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({
-						capabilities = capabilities,
-						on_attach = on_attach,
-						settings = opts.servers[server_name],
-					})
-				end,
-			})
-		end,
-	},
-
-	{
-		-- From LazyVim's lua/lazyvim/plugins/lsp/init.lua
-		"williamboman/mason.nvim",
-		cmd = "Mason",
-		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
-		opts = {
-			ensure_installed = {
-				"black",
-				"stylua",
-				"shfmt",
-			},
-		},
-		---@param opts MasonSettings | {ensure_installed: string[]}
-		config = function(_, opts)
-			require("mason").setup(opts)
-			local mr = require("mason-registry")
-			local function ensure_installed()
-				for _, tool in ipairs(opts.ensure_installed) do
-					local p = mr.get_package(tool)
-					if not p:is_installed() then
-						p:install()
-					end
-				end
-			end
-			if mr.refresh then
-				mr.refresh(ensure_installed)
-			else
-				ensure_installed()
-			end
-		end,
-	},
+  {
+    -- From LazyVim's lua/lazyvim/plugins/lsp/init.lua
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    opts = {
+      ensure_installed = {
+        "black",
+        "stylua",
+        "shfmt",
+      },
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
 }
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,18 +1,37 @@
 return {
-	-- Formatters
-	"jose-elias-alvarez/null-ls.nvim",
-	dependencies = { "mason.nvim" },
-	opts = function()
-		local nls = require("null-ls")
-		return {
-			-- next line is from LazyVim. Saving it as a comment in case it is useful for later.
-			-- root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-			sources = {
-				nls.builtins.formatting.stylua,
-				nls.builtins.formatting.black,
-				nls.builtins.formatting.shfmt,
-				nls.builtins.formatting.sql_formatter,
-			},
-		}
-	end,
+  -- Formatters
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = { "mason.nvim" },
+  opts = function()
+    local nls = require("null-ls")
+    return {
+      -- next line is from LazyVim. Saving it as a comment in case it is useful for later.
+      -- root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      sources = {
+        nls.builtins.formatting.stylua,
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.formatting.sql_formatter,
+      },
+      -- configure format on save
+      on_attach = function(current_client, bufnr)
+        if current_client.supports_method("textDocument/formatting") then
+          -- vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePost", {
+            -- group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({
+                filter = function(client)
+                  --  only use null-ls for formatting instead of lsp server
+                  return client.name == "null-ls"
+                end,
+                bufnr = bufnr,
+              })
+            end,
+          })
+        end
+      end,
+    }
+  end,
 }
