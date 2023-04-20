b0dcf36ed8be7ffd5a6e6b8aa2a22d23f0dc1444
David Rambo <davidrambo@mailfence.com>
2023-04-20 11:03:26 -0700

    Setup separate keymaps for lsp cmp and snippet cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -16,8 +16,8 @@ return {
           end,
         },
         mapping = cmp.mapping.preset.insert({
-          -- ["<C-n"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-          -- ["<C-p"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-n"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+          ["<C-p"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
@@ -26,19 +26,19 @@ return {
             select = false,
           }),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-n>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            -- if cmp.visible() then
+            --   cmp.select_next_item()
+            if luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
               fallback()
             end
           end, { "i", "s" }),
-          ["<C-p>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            -- if cmp.visible() then
+            --   cmp.select_prev_item()
+            if luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
               fallback()
