b19640bbb4b2404031b50eadce4721165449438c
David Rambo <davidrambo@mailfence.com>
2023-04-24 13:08:58 -0700

    Change autoformat on save to Pre write

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -21,7 +21,7 @@ return {
       on_attach = function(current_client, bufnr)
         if current_client.supports_method("textDocument/formatting") then
           -- vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePost", {
+          vim.api.nvim_create_autocmd("BufWritePre", {
             -- group = augroup,
             buffer = bufnr,
             callback = function()
