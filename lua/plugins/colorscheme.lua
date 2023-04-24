c3dd253c1797f88bf158da549408cf7e2678b21a
David Rambo <davidrambo@mailfence.com>
2023-04-24 10:26:10 -0700

    Configure catppuccin integrations

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -40,11 +40,31 @@ return {
         custom_highlights = {},
         integrations = {
           cmp = true,
+          fidget = true,
           gitsigns = true,
           illuminate = true,
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = false,
+          },
           leap = true,
           mason = true,
           mini = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+          },
           neogit = true,
           nvimtree = true,
           telescope = true,
