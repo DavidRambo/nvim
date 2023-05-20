0471048e27943d881f8934c785b15e59061d8c3f
David Rambo <davidrambo@mailfence.com>
2023-05-20 15:52:00 -0700

    Move options directly into configure

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -6,18 +6,28 @@ return {
   --   { "<leader>ti", "<cmd>IlluminateToggle<CR>", "[T]oggle [I]lluminate" },
   -- },
   opts = {
-    filetypes_denylist = {
-      "dirvish",
-      "fugitive",
-      "md",
-      "org",
-      "norg",
-    },
+    -- filetypes_denylist = {
+    --   "dirvish",
+    --   "fugitive",
+    --   "md",
+    --   "org",
+    --   "norg",
+    --   "NvimTree",
+    -- },
   },
   config = function(opts)
-    require("illuminate").configure(opts)
+    require("illuminate").configure({
+      filetypes_denylist = {
+        "dirvish",
+        "fugitive",
+        "md",
+        "org",
+        "norg",
+        "NvimTree",
+      },
+    })
 
-    vim.keymap.set("n", "<leader>ti", "<cmd>IlluminateToggle<CR>", { desc = "[T]oggle [I]lluminate"} )
+    vim.keymap.set("n", "<leader>ti", "<cmd>IlluminateToggle<CR>", { desc = "[T]oggle [I]lluminate" })
     vim.keymap.set("n", "<leader>tf", require("illuminate").toggle_freeze_buf, { desc = "[F]reeze Illuminate" })
   end,
 }
