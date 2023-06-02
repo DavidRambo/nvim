bb9edb8693120ede29b97aace1929160a7bf61ea
David Rambo <davidrambo@mailfence.com>
2023-06-02 13:31:13 -0700

    Add colorizer for tailwind css

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+      filetypes = {
+        "css",
+        "javascript",
+        "html",
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      opts.formatting = {
+        format = require("tailwindcss-colorizer-cmp").formatter,
+      }
+    end,
+  },
+}
