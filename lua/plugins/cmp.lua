81827fab41758e887905997124193b33a2a8db76
David Rambo <davidrambo@mailfence.com>
2024-04-05 14:54:45 -0700

    Go back to <[S]-Tab> for snippets b/c C-BS was messed up

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -36,7 +36,7 @@ return {
           }),
           ["<C-e>"] = cmp.mapping.abort(),
 
-          ["<C-l>"] = cmp.mapping(function(fallback)
+          ["<Tab>"] = cmp.mapping(function(fallback)
             if luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
             else
@@ -44,7 +44,7 @@ return {
             end
           end, { "i", "s" }),
 
-          ["<C-h>"] = cmp.mapping(function(fallback)
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
             if luasnip.jumpable(-1) then
               luasnip.jump(-1)
             else
