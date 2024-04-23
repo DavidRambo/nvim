efc9464a56f2f683d06cdd36f8355065682452c8
David Rambo <davidrambo@mailfence.com>
2024-04-23 09:27:00 -0700

    Add <C-g> as another Esc input

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,7 +11,7 @@ end
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
-vim.keymap.set({ "n", "v", "i", "x" }, "<C-g>", "<Esc>", { silent = true })
+vim.keymap.set({ "i", "v", "x" }, "<C-g>", "<Esc>", { silent = true })
 
 -- Remap for dealing with word wrap
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
