56ab23ebea1c6c7e0333b2b1f53c83f5f686cbcd
David Rambo <davidrambo@mailfence.com>
2024-04-27 10:18:00 -0700

    keymap for telescoping QMK keyboard files

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -28,7 +28,12 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
-vim.api.nvim_set_keymap("n", "<Up>", "v:count == 0 ? 'g<Up>' : '<Up>'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<Up>",
+  "v:count == 0 ? 'g<Up>' : '<Up>'",
+  { noremap = true, expr = true, silent = true }
+)
 vim.api.nvim_set_keymap(
   "n",
   "<Down>",
@@ -92,7 +97,12 @@ n_remap("<leader>wc", ":close<CR>")
 n_remap("<leader>cl", "<cmd>Lazy<CR>", "Lazy")
 
 -- See `:help telescope.builtin`
-vim.keymap.set("n", "<leader>fr", require("telescope.builtin").oldfiles, { desc = "[F]ind [R]ecently opened files" })
+vim.keymap.set(
+  "n",
+  "<leader>fr",
+  require("telescope.builtin").oldfiles,
+  { desc = "[F]ind [R]ecently opened files" }
+)
 vim.keymap.set("n", "<leader>/", function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
@@ -101,34 +111,80 @@ vim.keymap.set("n", "<leader>/", function()
   }))
 end, { desc = "[/] Fuzzily search in current buffer" })
 
-vim.keymap.set("n", "<leader>.", "<cmd>:lua require('config.tele').file_finder()<CR>", { desc = "Find Files" })
+vim.keymap.set(
+  "n",
+  "<leader>.",
+  "<cmd>:lua require('config.tele').file_finder()<CR>",
+  { desc = "Find Files" }
+)
 vim.keymap.set("n", "<leader>,", "<cmd>:Telescope buffers<CR>")
 n_remap("<leader>fb", ":Telescope file_browser path=%:p:h<CR>", "[F]ile [B]rowser")
-vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]ind [F]iles" })
+vim.keymap.set(
+  "n",
+  "<leader>ff",
+  require("telescope.builtin").find_files,
+  { desc = "[F]ind [F]iles" }
+)
 vim.keymap.set(
   "n",
   "<leader>fp",
   "<cmd>:lua require('config.tele').edit_nvim_conf()<CR>",
   { desc = "Neovim Config Files" }
 )
-vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
-vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
-vim.keymap.set("n", "<leader>sp", require("telescope.builtin").live_grep, { desc = "[S]earch [P]roject by grep" })
+vim.keymap.set(
+  "n",
+  "<leader>fq",
+  "<cmd>:lua require('config.tele').find_qmk()<CR>",
+  { desc = "QMK Keyboards" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>sh",
+  require("telescope.builtin").help_tags,
+  { desc = "[S]earch [H]elp" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>sw",
+  require("telescope.builtin").grep_string,
+  { desc = "[S]earch current [W]ord" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>sp",
+  require("telescope.builtin").live_grep,
+  { desc = "[S]earch [P]roject by grep" }
+)
 vim.keymap.set(
   "n",
   "<leader>sb",
   require("telescope.builtin").current_buffer_fuzzy_find,
   { desc = "[S]earch [B]uffer" }
 )
-vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+vim.keymap.set(
+  "n",
+  "<leader>sd",
+  require("telescope.builtin").diagnostics,
+  { desc = "[S]earch [D]iagnostics" }
+)
 vim.keymap.set("n", "<leader>sk", "<cmd>Telescope keymaps<CR>", { desc = "[S]earch [K]eymaps" })
 n_remap("<leader>ht", "<cmd>Telescope colorscheme<CR>")
-vim.keymap.set("n", "<leader>sc", "<cmd>Telescope colorscheme<CR>", { desc = "[S]earch [C]olorschemes " })
+vim.keymap.set(
+  "n",
+  "<leader>sc",
+  "<cmd>Telescope colorscheme<CR>",
+  { desc = "[S]earch [C]olorschemes " }
+)
 vim.keymap.set("n", "<leader>sr", "<cmd>Telescope resume<CR>", { desc = "[R]esume [S]earch" })
 vim.keymap.set("n", "<leader>st", "<cmd>TodoTelescope<CR>", { desc = "[S]earch [T]ODOs" })
 
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>dh", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set(
+  "n",
+  "<leader>dh",
+  vim.diagnostic.open_float,
+  { desc = "Open floating diagnostic message" }
+)
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
