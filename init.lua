8a444b234b0ffcf2b72da3e1e9b5c096aa4ebc37
David Rambo <davidrambo@mailfence.com>
2024-05-27 18:00:46 -0700

    Add TODO comment keymaps

--- init.lua
+++ init.lua
@@ -29,6 +29,21 @@ require("lazy").setup({
     event = "VeryLazy",
     config = function()
       require("todo-comments").setup()
+
+      vim.keymap.set("n", "]t", function()
+        require("todo-comments").jump_next()
+      end, { desc = "Next todo comment" })
+
+      vim.keymap.set("n", "[t", function()
+        require("todo-comments").jump_prev()
+      end, { desc = "Previous todo comment" })
+
+      vim.keymap.set(
+        "n",
+        "<leader>ft",
+        "<cmd>TodoTelescope<CR>",
+        { desc = "[F]ind [T]odo comments" }
+      )
     end,
   },
 
