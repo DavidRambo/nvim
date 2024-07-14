f2b6f0ca394a635a9f29de67297d3d78a7473ab6
David Rambo <davidrambo@mailfence.com>
2024-07-13 16:13:13 -0800

    Move plugins to separate files.

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,31 @@
+return {
+  "numToStr/Comment.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    ignore = "^$",
+    toggler = {
+      line = "<leader>cc",
+      block = "<leader>cb",
+    },
+    opleader = {
+      line = "<leader>c",
+      block = "<leader>b",
+    },
+    extra = {
+      -- Add comment on the line above
+      above = "<leader>cO",
+      -- Add comment on the line below
+      below = "<leader>co",
+      -- Add comment at the end of line
+      eol = "<leader>ca",
+    },
+    -- Enable keybindings
+    -- Note: If given `false` then the plugin won't create any mappings
+    -- mappings = {
+    --   -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+    --   basic = true,
+    --   -- Extra mapping; `gco`, `gcO`, `gcA`
+    --   extra = true,
+    -- },
+  },
+}
