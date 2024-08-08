0f6bdf48140c868bfdcf73cd32b305e9826f4b24
David Rambo <davidrambo@mailfence.com>
2024-08-08 11:23:15 -0700

    Unbind C-g to close telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,7 +14,6 @@ return {
               ["<C-u>"] = false,
               ["<C-d>"] = false,
               ["<esc>"] = require("telescope.actions").close,
-              ["<C-g>"] = require("telescope.actions").close,
             },
           },
           file_ignore_patterns = {
