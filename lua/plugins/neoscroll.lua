664638c743202242271b00e2767be000f6904675
David Rambo <davidrambo@mailfence.com>
2024-09-17 16:11:15 -0700

    fix: update neoscroll keybindings and function calls

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,22 +1,30 @@
 return {
   "karb94/neoscroll.nvim",
   config = function()
-    require("neoscroll").setup({
+    local neoscroll = require("neoscroll")
+    neoscroll.setup({
       -- Set any options as needed
     })
 
-    local t = {}
-    -- Syntax: t[keys] = {function, {function arguments}}
-    t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "200", [["sine"]] } }
-    t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "200", [["sine"]] } }
-    t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "400", [["sine"]] } }
-    t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "400", [["sine"]] } }
-    t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
-    t["<C-e>"] = { "scroll", { "0.10", "false", "100" } }
-    t["zt"] = { "zt", { "250" } }
-    t["zz"] = { "zz", { "250" } }
-    t["zb"] = { "zb", { "250" } }
+    local keymap = {
 
-    require("neoscroll.config").set_mappings(t)
+      -- Syntax: t[keys] = {function, {function arguments}}
+      ["<C-u>"] = function()
+        neoscroll.ctrl_u({ duration = 200, easing = "sine" })
+      end,
+      ["<C-d>"] = function()
+        neoscroll.ctrl_d({ duration = 200, easing = "sine" })
+      end,
+      ["<C-y>"] = function()
+        neoscroll.scroll(-0.1, { move_cursor = false, duration = 100 })
+      end,
+      ["<C-e>"] = function()
+        neoscroll.scroll(0.1, { move_cursor = false, duration = 100 })
+      end,
+    }
+    local modes = { "n", "v", "x" }
+    for key, func in pairs(keymap) do
+      vim.keymap.set(modes, key, func)
+    end
   end,
 }
