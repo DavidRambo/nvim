9f0dcdc8b8b5abca68243f57f2c1ab983284da06
David Rambo <davidrambo@mailfence.com>
2024-11-15 17:33:43 -0800

    turn off highlighting by default

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,20 +1,5 @@
 return {
   "RRethy/vim-illuminate",
-  -- cmd = "IlluminateToggle",
-  -- keys = {
-  --   -- { "<leader>ti", require("illuminate").toggle(), "[T]oggle [I]lluminate" }
-  --   { "<leader>ti", "<cmd>IlluminateToggle<CR>", "[T]oggle [I]lluminate" },
-  -- },
-  opts = {
-    -- filetypes_denylist = {
-    --   "dirvish",
-    --   "fugitive",
-    --   "md",
-    --   "org",
-    --   "norg",
-    --   "NvimTree",
-    -- },
-  },
   config = function(opts)
     require("illuminate").configure({
       filetypes_denylist = {
@@ -25,9 +10,32 @@ return {
         "norg",
         "NvimTree",
       },
+      min_count_to_highlight = 2,
+      modes_denylist = { "i" },
     })
 
-    vim.keymap.set("n", "<leader>ti", "<cmd>IlluminateToggle<CR>", { desc = "[T]oggle [I]lluminate" })
-    vim.keymap.set("n", "<leader>tf", require("illuminate").toggle_freeze_buf, { desc = "[F]reeze Illuminate" })
+    vim.keymap.set(
+      "n",
+      "<leader>ti",
+      -- "<cmd>IlluminateToggle<CR>",
+      require("illuminate").toggle_visibility_buf,
+      { desc = "[T]oggle [I]lluminate" }
+    )
+    vim.keymap.set(
+      "n",
+      "<leader>tf",
+      require("illuminate").toggle_freeze_buf,
+      { desc = "[F]reeze Illuminate" }
+    )
+
+    -- Autocommand to turn off highlighting by default.
+    local augroup = "vim-illuminate-autocmds"
+    vim.api.nvim_create_augroup(augroup, { clear = true })
+    vim.api.nvim_create_autocmd("BufRead", {
+      group = augroup,
+      callback = function()
+        require("illuminate").invisible_buf()
+      end,
+    })
   end,
 }
