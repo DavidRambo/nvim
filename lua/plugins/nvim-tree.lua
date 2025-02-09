e8ce8151529091c027a2b514522a7688fef8cfd7
David Rambo <davidrambo@mailfence.com>
2025-02-09 15:11:24 -0800

    disable nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,37 +1,40 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  keys = {
-    { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "NvimTree" },
-  },
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup({
-      renderer = {
-        icons = {
-          show = {
-            folder = true,
+  {
+    "nvim-tree/nvim-tree.lua",
+    enabled = false,
+    version = "*",
+    lazy = false,
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "NvimTree" },
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        renderer = {
+          icons = {
+            show = {
+              folder = true,
+            },
+            -- glyphs = {
+            --   folder = {
+            --     arrow_closed = "﬌",
+            --     arrow_open = "",
+            --   },
+            -- },
           },
-          -- glyphs = {
-          --   folder = {
-          --     arrow_closed = "﬌",
-          --     arrow_open = "",
-          --   },
-          -- },
+          indent_markers = {
+            enable = false,
+          },
         },
-        indent_markers = {
-          enable = false,
+        filters = {
+          dotfiles = true,
         },
-      },
-      filters = {
-        dotfiles = true,
-      },
-      view = {
-        side = "right",
-      },
-    })
-  end,
+        view = {
+          side = "right",
+        },
+      })
+    end,
+  },
 }
