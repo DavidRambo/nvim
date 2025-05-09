ee4cd3501fdc90092110712aa9da30e68a6320de
David Rambo <davidrambo@mailfence.com>
2025-05-09 13:08:25 -0700

    update to Mason v2 and vim.lsp.config
    Converted nvim v10 lspconfig to nvim v11 version:
    vim.lsp.config('server_name', {})
    
    Moved the on_attach function to an autocmd.
    
    Commented out mason.nvim's list of ensure_installed servers, since this
    is done through mason_lspconfig.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -65,73 +65,69 @@ return {
       },
     },
     config = function(_, opts)
-      local on_attach = function(client, bufnr)
-        -- Do not attach the server to Navbuddy if it does not support documentSymbols.
-        if client.server_capabilities.documentSymbolProvider then
-          require("nvim-navbuddy").attach(client, bufnr)
-        end
+      -- See: https://neovim.io/doc/user/lsp.html#lsp-attach
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("my.lsp", {}),
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+          local bufnr = assert(args.buf)
 
-        local nmap = function(keys, func, desc)
-          if desc then
-            desc = "LSP: " .. desc
+          if client:supports_method("textDocument/documentSymbol") then
+            require("nvim-navbuddy").attach(client, bufnr)
           end
 
-          vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-        end
+          local nmap = function(keys, func, desc)
+            if desc then
+              desc = "LSP: " .. desc
+            end
 
-        nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-        nmap("<C-c>", vim.lsp.buf.code_action, "[C]ode [A]ction")
+            vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+          end
 
-        nmap("<leader>gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        nmap("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        nmap("<leader>gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-        nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-        nmap(
-          "<leader>ds",
-          require("telescope.builtin").lsp_document_symbols,
-          "[D]ocument [S]ymbols"
-        )
-        -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          nmap("<C-c>", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-        -- See `:help K` for why this keymap
-        nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-        vim.keymap.set(
-          { "n", "i" },
-          "<C-k>",
-          vim.lsp.buf.signature_help,
-          { buffer = bufnr, desc = "Signature Help" }
-        )
-        -- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+          nmap("<leader>gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+          nmap("<leader>gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          nmap("<leader>gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+          nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+          nmap(
+            "<leader>ds",
+            require("telescope.builtin").lsp_document_symbols,
+            "[D]ocument [S]ymbols"
+          )
+          -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
-        -- Lesser used LSP functionality
-        nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-        -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-        -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-        -- nmap('<leader>wl', function()
-        --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, '[W]orkspace [L]ist Folders')
+          -- See `:help K` for why this keymap
+          nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+          vim.keymap.set(
+            { "n", "i" },
+            "<C-k>",
+            vim.lsp.buf.signature_help,
+            { buffer = bufnr, desc = "Signature Help" }
+          )
+          -- nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
 
-        -- Create a command `:Format` local to the LSP buffer
-        -- vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        --   vim.lsp.buf.format()
-        -- end, { desc = "Format current buffer with LSP" })
-        -- nmap("<leader>bf", "<cmd>Format<CR>", "[B]uffer [F]ormat")
-      end
+          -- Lesser used LSP functionality
+          nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+          -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+          -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+          -- nmap('<leader>wl', function()
+          --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          -- end, '[W]orkspace [L]ist Folders')
+
+          -- Create a command `:Format` local to the LSP buffer
+          -- vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+          --   vim.lsp.buf.format()
+          -- end, { desc = "Format current buffer with LSP" })
+          -- nmap("<leader>bf", "<cmd>Format<CR>", "[B]uffer [F]ormat")
+        end,
+      })
 
       -- Add a border to the hover frame.
       -- FIX: Tried to replace deprecated API, but this doesn't work.
       -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.buf.hover({ border = "rounded" })
       -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.buf.hover({ border = "rounded" })
-
-      -- require("lsp_signature").on_attach({
-      --   bind = true, -- This is mandatory, otherwise border config won't get registered.
-      --   handler_opts = {
-      --     border = "rounded",
-      --   },
-      --   hint_enable = false,
-      --   hint_prefix = "",
-      -- })
-      -- , bufnr) -- bufnr only required when using toggle_key function
 
       vim.diagnostic.config(opts.diagnostics)
 
@@ -142,25 +138,17 @@ return {
       -- Ensure the servers above are installed
       local mason_lspconfig = require("mason-lspconfig")
 
-      -- mason_lspconfig.setup({
-      --   ensure_installed = vim.tbl_keys(opts.servers),
-      -- })
-
-      mason_lspconfig.setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = opts.servers[server_name],
-          })
-        end,
+      mason_lspconfig.setup({
+        ensure_installed = vim.tbl_keys(opts.servers),
+        automatic_enable = true,
+        -- on_attach = on_attach,
       })
 
       -- Moved out of mason_lspconfig handlers loop because diagnostics
       -- were not being disabled.
-      require("lspconfig").jedi_language_server.setup({
+      vim.lsp.config("jedi_language_server", {
         capabilities = capabilities,
-        on_attach = on_attach,
+        -- on_attach = on_attach,
         init_options = {
           diagnostics = { enable = false },
         },
@@ -169,9 +157,9 @@ return {
       -- Workaround for warning when using clang-format with clangd lsp.
       capabilities.offsetEncoding = { "utf-16" }
 
-      require("lspconfig").clangd.setup({
+      vim.lsp.config("clangd", {
         capabilities = capabilities,
-        on_attach = on_attach,
+        -- on_attach = on_attach,
         init_options = {
           diagnostics = { enable = false },
         },
@@ -185,25 +173,21 @@ return {
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     opts = {
-      ensure_installed = {
+      --[[ ensure_installed = {
         "beautysh",
-        -- "black",
         "clangd",
         "emmet-language-server",
-        -- "eslint-lsp",
         "gopls",
         "jedi-language-server",
         "lua-language-server",
-        -- "mypy",
         "prettierd",
         "rnix-lsp",
         "ruff", -- for formatting with Conform.nvim
         "sql-formatter",
         "stylua",
-        "typescript-language-server",
-      },
+      }, ]]
     },
-    ---@param opts MasonSettings | {ensure_installed: string[]}
+    --[[ ---@param opts MasonSettings | {ensure_installed: string[]}
     config = function(_, opts)
       require("mason").setup(opts)
 
@@ -223,6 +207,6 @@ return {
       else
         ensure_installed()
       end
-    end,
+    end, ]]
   },
 }
