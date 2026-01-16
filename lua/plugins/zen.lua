faa2f1244af61db792b6061c86f5d43ced767e4f
David Rambo <davidrambo@mailfence.com>
2026-01-15 22:09:56 -0800

    zen mode: disable Twilight

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -34,7 +34,7 @@ return {
           ruler = false, -- disables the ruler text in the cmd line area
           showcmd = false, -- disables the command in the last line of the screen
         },
-        twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+        twilight = { enabled = false }, -- enable to start Twilight when zen mode opens
         gitsigns = { enabled = false }, -- disables git signs
         tmux = { enabled = false }, -- disables the tmux statusline
       },
