e7a81bb188a83850dca6edbb780eb0e24c55fe6e
David Rambo <davidrambo@mailfence.com>
2026-01-15 22:09:33 -0800

    update leap to codeberg repo

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -1,9 +1,9 @@
 return {
-  "ggandor/leap.nvim",
+  "https://codeberg.org/andyg/leap.nvim",
   -- event = "VeryLazy",
   config = function()
-    vim.keymap.set({'n', 'x', 'o'}, 's',  '<Plug>(leap-forward)')
-    vim.keymap.set({'n', 'x', 'o'}, 'S',  '<Plug>(leap-backward)')
-    vim.keymap.set('n',             'gs', '<Plug>(leap-from-window)')
+    vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+    vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+    vim.keymap.set("n", "gs", "<Plug>(leap-from-window)")
   end,
 }
