9ceab71e70ec396a4ee275f2dbfc348a6afeac88
David Rambo <davidrambo@mailfence.com>
2026-05-03 20:12:53 -0700

    mason-org urls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,8 +5,8 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
 
       -- Useful status updates for LSP
       -- Note: `opts = {}` is the same as calling `require('fidget').setup({})`
